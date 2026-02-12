8c609af535b0bd5eba50584c6d59dcc12e3054c7
Vinícius Diller <viniciusdiller@hotmail.com>
2026-02-12 13:32:41 -0300

    git ingore

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("3be594114a8d435b")
+/******/ 		__webpack_require__.h = () => ("2d1471d9fef19ae5")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ff2ed44eae6ac148"; }
+/******/ 		__webpack_require__.h = function() { return "b398964e015beaa2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
